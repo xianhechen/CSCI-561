0c21c22439c8a582bf3fe32bfeb9efa4ef95fb44
xianhechen <chenxianhe@icloud.com>
2017-09-16 01:27:58 -0700

    Update homework.java

--- CSCI561_HW1/src/homework.java
+++ CSCI561_HW1/src/homework.java
@@ -3,8 +3,8 @@ import sun.misc.Queue;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Stack;
 
 public class homework {
@@ -13,8 +13,18 @@ public class homework {
     public static int size = 0;
     public static int lizards = 0;
 
-    public static int line_number = 0;
+    public static int line_number = 1;
     public static int[][] result;
+    public static int[][] new_lizards_locations;
+
+    public static int[][] lizards_locations;
+    public static double currentSystemTemperature = 30.0;
+    public static double freezingTemperature = 0.0;
+    public static double currentStabilizer = 5.0;
+    public static double currentSystemEnergy = 0.0;
+    public static double coolingFactor = 0.05;
+    public static double stabilizingFactor = 1.08;
+
 
     class Root {
         int[][] board;
@@ -61,9 +71,9 @@ public class homework {
             int d = curr_root.depth();
             int w = curr_root.length();
             int l = curr_root.lizard_num();
-            System.out.println("Lizards in:"+l);
+//            System.out.println("Lizards in:"+l);
             if (l == lizards) {
-                System.out.println("asdf:"+l);
+//                System.out.println("asdf:"+l);
                 //result = curr_root.board();
                 return curr_root.board();
             }
@@ -175,9 +185,6 @@ public class homework {
         return empty;
     }
 
-
-
-
     boolean isSafe(int[][] board, int y, int x) {
         // up
         for (int i = y; i >= 0; i--) {
@@ -263,42 +270,240 @@ public class homework {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+    // flip the coin function
+    boolean probability(double temperature, double delta) {
+        System.out.println("6");
+        if (delta < 0) {
+            return true;
+        }
+        double E = Math.exp(-delta / temperature);
+        double R = Math.random();
+        if (R < E) {
+            return true;
+        }
+        return false;
+    }
+
+    // number of conflicts needs to be as small as possible
+    int countConflicts(int [][] board) {
+        System.out.println("5");
+        int conflicts = 0;
+        for (int row = 0; row != board.length; row++) {
+            for (int col = 0; col != board[row].length; col++) {
+                if (board[row][col] == 1) {
+                    board[row][col] = 0;
+                    if (!isSafe(board, row, col)) {
+//                        System.out.println("conflicting row: "+row+" col: "+col+" safe: "+isSafe(board, row, col));
+                        conflicts++;
+                        board[row][col] = 1;
+                    }
+                    board[row][col] = 1;
+
+                }
+            }
+        }
+        return conflicts;
+    }
+
+    // generates random lizard locations
+    void randomPositionsGenerator() {
+        System.out.println("4");
+        for (int i = 0; i < lizards; i++) {
+            int row = 0;
+            int col = 0;
+            int[] random_position = randomPositionGenerator();
+            row = random_position[0];
+            col = random_position[1];
+
+            while (result[row][col] != 0) {
+                //generate again
+                random_position = randomPositionGenerator();
+                row = random_position[0];
+                col = random_position[1];
+            }
+
+            lizards_locations[i][0] = row;
+            lizards_locations[i][1] = col;
+            result[row][col] = 1;
+        }
+    }
+
+    // generates random lizard location
+    int[] randomPositionGenerator() {
+        // generate random numbers between 1 and size number
+        int row = (int )(Math.random() * (size-1) + 0);
+        int col = (int )(Math.random() * (size-1) + 0);
+        return new int[] {row, col};
+    }
+
+
+
+
+
+    int[][] generateNeighbor(int[][] board) {
+        // take our current board, take a random queen, make a single step.
+        // need a list of lizards to track all lizards' locations
+        // take a random lizard
+        int changingLizard = (int)(Math.random() * (lizards-1) + 0);
+
+        // current lizards
+        new_lizards_locations = deepCopy(lizards_locations);
+
+        boolean repetitions = true;
+        while (repetitions) {
+            int old_row = new_lizards_locations[changingLizard][0];
+            int old_col = new_lizards_locations[changingLizard][1];
+            System.out.println("2");
+            new_lizards_locations[changingLizard][0] = (new_lizards_locations[changingLizard][0] + ((int)(Math.random() * (size - 1))) )% size;
+            new_lizards_locations[changingLizard][1] = (new_lizards_locations[changingLizard][1] + ((int)(Math.random() * (size - 1))) )% size;
+//            System.out.println("row: "+new_lizards_locations[changingLizard][0]+"col: "+new_lizards_locations[changingLizard][1]);
+            int new_row = new_lizards_locations[changingLizard][0];
+            int new_col = new_lizards_locations[changingLizard][1];
+            if (result[new_row][new_col] == 0) {
+                result[old_row][old_col] = 0;
+                result[new_row][new_col] = 1;
+                repetitions = false;
+
+            } else {
+                new_lizards_locations[changingLizard][0] = old_row;
+                new_lizards_locations[changingLizard][1] = old_col;
+            }
+
+        }
+        for (int[] row : lizards_locations)
+        {
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("___________________________");
+        for (int[] row : new_lizards_locations)
+        {
+            System.out.println(Arrays.toString(row));
+        }
+        //return countConflicts(result); //return number of conflicts
+        return board;
+    }
+
+    void acceptNext() {
+        System.out.println("3");
+        for (int row = 0; row != result.length; row++) {
+            for (int col = 0; col != result[row].length; col++) {
+                if (result[row][col] == 1) {
+                    result[row][col] = 0;
+                }
+            }
+        }
+//        System.out.println("size: "+new_lizards_locations.length);
+        for (int i = 0; i < lizards; i++) {
+            //lizards_locations[i] = new int[]{new_lizards_locations[i][0], new_lizards_locations[i][1]};
+//            result[lizards_locations[i][0]][lizards_locations[i][1]] = 0;
+            result[new_lizards_locations[i][0]][new_lizards_locations[i][1]] = 1;
+        }
+    }
+
+
+    boolean simulatedAnnealing() {
+        //while (currentSystemTemperature > freezingTemperature) {
+//            double newEnergy = generateNext(),
+//                    energyDelta = newEnergy - currentSystemEnergy;
+//            if (probability(currentSystemTemperature, energyDelta)) {
+//                acceptNext();
+//                currentSystemEnergy = newEnergy;
+//            }
+//            currentSystemTemperature = currentSystemTemperature - coolingFactor;
+//            currentStabilizer = currentStabilizer * stabilizingFactor;
+//        int curr_cost = countConflicts(result);
+//        int[][] temp_board = deepCopy(result);
+//        int new_cost = generateNext();
+        return false;
+    }
+
+//
+//    simulated-annealing(initial solution)
+//    let solution be initial
+//    let t be an initial temperature
+//    until t is almost zero
+//          let neighbor be a random neighbor of solution
+//          if the cost of neighbor is less than the cost of solution
+//              let solution be neighbor
+//              stop if the cost is now 0
+//          otherwise
+//              let c be the cost increase
+//              compute p = e^(-c/t)
+//              with probability p, let solution be neighbor
+//          multiply t by a decay rate
+//     return solution
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String args[]) throws IOException, InterruptedException {
 
-//        try {
-//            BufferedReader in = new BufferedReader(new FileReader("input.txt"));
-//            String str;
-//            while ((str = in.readLine()) != null) {
-//                line_number ++;
-//                if (line_number == 1) {
-//                    method = str;
-//                }
-//                else if (line_number == 2) {
-//                    size = Integer.valueOf(str);
-//                }
-//                else if (line_number == 3) {
-//                    lizards = Integer.valueOf(str);
-//                }
-//                else {
-//                    // System.out.println(str);
-//                    // store the nursery as a matrix?
-//                }
-//            }
-//            System.out.println(method);
-//            System.out.println(size);
-//            System.out.println(lizards);
-//            in.close();
-//        } catch (IOException e) {
-//        }
+//        int[][] initial_map = new int[][];
+        int[][] initial_map = new int[0][];
 
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+            String str;
+            while ((str = in.readLine()) != null) {
 
-        int row_len = 8;
-        int col_len = 8;
-        int n = 9;
-        lizards = n;
-        int[][] initial_map = new int[row_len][col_len];
-        initial_map[3][4] = 2;
-        initial_map[5][5] = 2;
+                if (line_number == 1) {
+                    method = str;
+                }
+                else if (line_number == 2) {
+                    size = Integer.valueOf(str);
+                    initial_map = new int[size][size];
+                }
+                else if (line_number == 3) {
+                    lizards = Integer.valueOf(str);
+                }
+                else {
+                    // store the nursery as a matrix?
+                    for (int i = 0; i != size; i++) {
+                        initial_map[line_number-4][i] = Integer.parseInt(str.valueOf(str.charAt(i)));
+                    }
+                }
+                line_number ++;
+            }
+            System.out.println(method);
+            System.out.println(size);
+            System.out.println(lizards);
+            in.close();
+        } catch (IOException e) {
+        }
+
+//        initial_map[3][4] = 2;
+//        initial_map[5][5] = 2;
 
         result = initial_map;
         for (int[] row : initial_map)
@@ -309,13 +514,73 @@ public class homework {
         System.out.println("____________________________");
 
         homework hw = new homework();
-        int[][] temp = hw.bfs(initial_map,0,0,0);
-//        System.out.println(temp);
-        for (int[] row : temp)
-        {
-            System.out.println(Arrays.toString(row));
+
+
+        if (method.equals("DFS")) {
+            result = hw.dfs(initial_map,0,0,0);
+
         }
 
+        else if (method.equals("BFS")) {
+            result = hw.bfs(initial_map,0,0,0);
+        }
+
+        else if (method.equals("SA")) {
+//            result = hw.simulatedAnnealing();
+            lizards_locations = new int[lizards][2];
+            hw.randomPositionsGenerator();
+            for (int[] row : result)
+            {
+                System.out.println(Arrays.toString(row));
+            }
+
+            boolean i = hw.simulatedAnnealing();
+            System.out.println(i);
+
+
+
+            for (int[] row : result)
+            {
+                System.out.println(Arrays.toString(row));
+            }
+
+//            boolean done = false;
+//
+//            while (!done) {
+//                System.out.println("1");
+//                done = hw.simulatedAnnealing();
+//            }
+
+
+
+
+//            result[0][0] = 1;
+//            result[1][2] = 1;
+//            result[1][4] = 1;
+//            result[1][6] = 1;
+//            result[2][4] = 1;
+//            result[4][5] = 1;
+
+
+        }
+
+        try{
+            PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+            writer.println(method);
+            writer.println(size);
+            writer.println(lizards);
+            for (int[] row : result)
+            {
+                String result_row = "";
+                for (int i = 0; i != row.length; i++) {
+                    result_row += row[i];
+                }
+                writer.println(result_row);
+            }
+            writer.close();
+        } catch (IOException e) {
+
+        }
 
     }
 
